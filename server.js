0448db8f383af158282013cb606f2c4949c37ea3
RedsSnk <101672482+RedsSnk@users.noreply.github.com>
2025-07-02 07:41:01 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*', // Change this to your domain if needed
-    methods: ['GET', 'POST']
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
   }
 });
 
-app.use(cors());
-app.use(express.static('public'));
+app.use(cors({ origin: '*', credentials: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
